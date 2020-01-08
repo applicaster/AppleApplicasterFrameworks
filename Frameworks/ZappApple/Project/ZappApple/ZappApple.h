0c4221c10aca2f999c8e96ff74d01eddf74b05cc
kononenkoAnton <a.kononenko@applicaster.com>
2020-01-08 12:59:14 -0500

    Add documentation logic

---
+++ Frameworks/ZappApple/Project/ZappApple/ZappApple.h
@@ -0,0 +1,22 @@
+//
+//  ZappApple.h
+//  ZappApple
+//
+//  Created by Anton Kononenko on 1/8/20.
+//  Copyright © 2020 Applicaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZappApple.
+FOUNDATION_EXPORT double ZappAppleVersionNumber;
+
+//! Project version string for ZappApple.
+FOUNDATION_EXPORT const unsigned char ZappAppleVersionString[];
+
+#import "CSSubscriptionManager.h"
+#import "Keychain.h"
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZappApple/PublicHeader.h>
+
+
