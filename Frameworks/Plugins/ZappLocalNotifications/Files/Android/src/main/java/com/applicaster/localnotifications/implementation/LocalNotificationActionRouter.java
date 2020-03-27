8c214afe3b4c272e32db4825af69fe768e4bfca0
Alex <24409942+inckie@users.noreply.github.com>
2020-03-27 11:06:50 -0400

    Android implementation

---
+++ Frameworks/Plugins/ZappLocalNotifications/Files/Android/src/main/java/com/applicaster/localnotifications/implementation/LocalNotificationActionRouter.java
@@ -0,0 +1,27 @@
+package com.applicaster.localnotifications.implementation;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This entire class is needed to dismiss notification on action button click
+ */
+public class LocalNotificationActionRouter extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int notificationId = intent.getIntExtra(NotificationBuilder.NOTIFICATION_ID_EXTRA, 0);
+        if(0 != notificationId) {
+            // hack to dismiss notification when action button is clicked
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.cancel(notificationId);
+        }
+        Intent routedIntent = intent.getParcelableExtra(NotificationBuilder.WRAPPED_INTENT_EXTRA);
+        if(null == routedIntent) {
+            throw new RuntimeException("Application missing launch intent");
+        }
+        context.startActivity(routedIntent);
+    }
+}
