6ecf873900f2dea1d77e81b08a205f5a9b967d73
Alex Zchut <a.zchut@applicaster.com>
2020-03-31 15:09:30 +0300

    adding changes for playing info to set image and description of atom entry

--- Frameworks/Plugins/ZappAppleVideoNowPlayingInfo/Files/tvOS/ZPAppleVideoNowPlayingInfo+PlayerObserverProtocol.swift
+++ Frameworks/Plugins/ZappAppleVideoNowPlayingInfo/Files/tvOS/ZPAppleVideoNowPlayingInfo+PlayerObserverProtocol.swift
@@ -25,12 +25,33 @@ extension ZPAppleVideoNowPlayingInfo {
 
         var metadataItems: [AVMetadataItem] = [AVMetadataItem]()
         metadataItems.append(self.metadataItem(identifier: AVMetadataIdentifier.commonIdentifierTitle,
-                                          value: title))
+                                               value: title))
         metadataItems.append(self.metadataItem(identifier: AVMetadataIdentifier(rawValue: AVKitMetadataIdentifierExternalContentIdentifier),
-                                          value: contentId))
+                                               value: contentId))
         metadataItems.append(self.metadataItem(identifier: AVMetadataIdentifier(rawValue: AVKitMetadataIdentifierPlaybackProgress),
-                                          value: NSNumber(integerLiteral: 0)))
+                                               value: NSNumber(integerLiteral: 0)))
 
+        
+        //image
+        if let mediaGroup = entry[ItemMetadata.media_group] as? [[AnyHashable: Any]],
+            let mediaItem = mediaGroup.first?[ItemMetadata.media_item] as? [[AnyHashable: Any]],
+            let src = mediaItem.first?[ItemMetadata.src] as? String,
+            let key = mediaItem.first?["key"] as? String, key == "image_base",
+            let url = URL(string: src) {
+            
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    metadataItems.append(self.metadataArtworkItem(image: image))
+                }
+            }
+        }
+        
+        //description
+        if let summary = entry[ItemMetadata.summary] as? (NSCopying & NSObjectProtocol) {
+            metadataItems.append(self.metadataItem(identifier: AVMetadataIdentifier.commonIdentifierDescription,
+                                                   value: summary))
+        }
+        
         avPlayer?.currentItem?.externalMetadata = metadataItems
     }
 }
