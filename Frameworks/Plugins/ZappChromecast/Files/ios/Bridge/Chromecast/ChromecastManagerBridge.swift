de53bd34bc0f7eca13ce06ffe1e278d2df75d91f
Alex Zchut <a.zchut@applicaster.com>
2020-05-05 16:19:18 +0300

    update

--- Frameworks/Plugins/ZappChromecast/Files/ios/Bridge/Chromecast/ChromecastManagerBridge.swift
+++ Frameworks/Plugins/ZappChromecast/Files/ios/Bridge/Chromecast/ChromecastManagerBridge.swift
@@ -22,41 +22,39 @@ class ChromecastManager: NSObject, RCTBridgeModule {
     public class func requiresMainQueueSetup() -> Bool {
         return true
     }
-   
+
     @objc public func play() {
         DispatchQueue.main.async {
             self.pluginInstance?.play()
         }
     }
-    
+
     @objc public func stop() {
         DispatchQueue.main.async {
             self.pluginInstance?.stop()
         }
     }
-    
+
     @objc public func pause() {
         DispatchQueue.main.async {
             self.pluginInstance?.pause()
         }
     }
-    
+
     @objc public func seek(_ playPosition: Int) {
         DispatchQueue.main.async {
             self.pluginInstance?.seek(playPosition)
-
         }
     }
-    
+
     @objc public func setVolume(_ volume: CGFloat) {
         DispatchQueue.main.async {
             self.pluginInstance?.setVolume(Float(volume))
         }
     }
-    
+
     @objc public func showIntroductoryOverlay() {
         DispatchQueue.main.async {
-            self.pluginInstance?.shouldPresentIntroductionScreen = true
             self.pluginInstance?.presentIntroductionScreenIfNeeded()
         }
     }
@@ -66,14 +64,14 @@ class ChromecastManager: NSObject, RCTBridgeModule {
             self.pluginInstance?.presentExtendedPlayerControls()
         })
     }
-    
+
     @objc public func castMedia(_ params: NSDictionary) {
         DispatchQueue.main.async {
             self.pluginInstance?.prepareToPlay(playableItems: [params],
                                                playPosition: 0)
         }
     }
-    
+
     @objc public func hasConnectedCastSession(_ resolver: @escaping RCTPromiseResolveBlock,
                                               rejecter: @escaping RCTPromiseRejectBlock) {
         DispatchQueue.main.async {
@@ -81,11 +79,11 @@ class ChromecastManager: NSObject, RCTBridgeModule {
                 rejecter("0", "plugin not available", nil)
                 return
             }
-            
+
             resolver(pluginInstance.hasConnectedCastSession())
         }
     }
-    
+
     @objc public func getCastState(_ resolver: @escaping RCTPromiseResolveBlock,
                                    rejecter: @escaping RCTPromiseRejectBlock) {
         DispatchQueue.main.async {
@@ -93,11 +91,11 @@ class ChromecastManager: NSObject, RCTBridgeModule {
                 rejecter("0", "plugin not available", nil)
                 return
             }
-            
+
             resolver(pluginInstance.getCurrentCastState())
         }
     }
-    
+
     fileprivate var pluginInstance:ChromecastAdapter? {
         guard let chromecastPlugin = FacadeConnector.connector?.pluginManager?.getProviderInstance(identifier: pluginIdentifier) as? ChromecastAdapter else {
             return nil
